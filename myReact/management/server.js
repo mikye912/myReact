6e8328aac2abbf9753eda300a6728f64f87135dc
KMK <mikye912@gmail.com>
2022-01-23 13:52:34 +0900

    Node.js Express

--- myReact/management/server.js
+++ myReact/management/server.js
@@ -6,8 +6,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/api/hello', (req, res) => {
+app.get('/api/hello', (req,res) => {
     res.send({message: 'Hello Express!'});
 });
-
-app.listen(port, () => console.log('Listening on port ${port}'));
+app.listen(port, () => console.log(`Listening on port ${ port }`));
